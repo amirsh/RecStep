dea17cc107edfa73e5b4447bfbdf9b8456033e2a
Amir Shaikhha <amir.shaikhha@ed.ac.uk>
2023-02-01 12:52:09 +0000

    Prefix sum and experimentation scripts

---
+++ run-experiments.sh
@@ -0,0 +1,41 @@
+#Datasets="wiki"
+Datasets="vec"
+#Datasets="wiki epinions twitter"
+#Programs="reach"
+#Programs="cc"
+#Programs="presum_stratified"
+Programs="presum"
+# Programs="cc reach sssp"
+
+function qry ()
+{
+	case "$1" in
+		"reach") Sfx="" ;;
+		"cc") Sfx="" ;;
+		"sssp") Sfx="-w" ;;
+		*) echo "bad program";;
+	esac
+	return 0
+}
+
+function runrs ()
+{
+	time python3 interpreter.py --program ./benchmark_datalog_programs/$1$Opt.datalog
+	echo "select count(*) from $1;" | python3 quickstep_shell.py --mode interactive
+}
+
+for P in $Programs
+do
+	qry $P
+	for D in $Datasets
+	do
+		cp ../datasets/$D/$D$Sfx.csv ./Input/arc.csv
+		echo " BENCHMARKING $P on $D" ;
+		#cp Config.json.t Config.json
+		#Opt=""
+		#runrs $P
+		#cp Config.json.f Config.json
+		Opt="-opt"
+		runrs $P
+	done
+done
